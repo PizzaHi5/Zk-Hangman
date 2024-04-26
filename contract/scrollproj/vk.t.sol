ba5800b8b7e1e95a9bcf3c085ede912c103f73e1
PizzaHi5 <pizzahi5@gmail.com>
2024-04-26 16:03:28 -0700

    working in progress

---
+++ contract/scrollproj/vk.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import {Test, console2} from "forge-std/Test.sol";
+//import {Counter} from "../src/Counter.sol";
+
+contract ponktest is Test {
+    
+}
